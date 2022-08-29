1b7958fe263804467398e4c9db9399e6ab85ec83
BAUHAUS <patrickfromgnomedk@gmail.com>
2022-08-29 08:09:22 +0200

    Update Token.cs

--- ApriSiSteam/Token.cs
+++ ApriSiSteam/Token.cs
@@ -4,7 +4,6 @@ namespace ApriSiSteam
 {
     public static class Token
     {
-        // Wish there was a better way, there probably is but not sure how
         public static string GetKey() => File.ReadAllText("token.txt");
     }
 }
