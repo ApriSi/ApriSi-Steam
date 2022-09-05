e6d99a0d8b4d47aa9252462ac29e7eca918128d7
BAUHAUS <patrickfromgnomedk@gmail.com>
2022-09-05 12:58:39 +0200

    Delete ApriSiSteam directory

--- ApriSiSteam/Models/Game.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ApriSiSteam.Models;
-public class Game
-{
-    public int? Appid { get; set; }
-    public string? Name { get; set; }
-    public string? ImagePath { get; set; }
-}
--- ApriSiSteam/Models/OwnedGames.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ApriSiSteam.Models;
-public class OwnedGames
-{
-    public int? Game_count { get; set; }
-    public Game[]? Games { get; set; }
-}
